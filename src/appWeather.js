d7a1cd31998ad48fa0a1997d38957e323e5a4988
Trunova Yulia <103053349+Yukkaf1@users.noreply.github.com>
2023-03-01 10:51:37 +0100

    date

--- src/appWeather.js
+++ src/appWeather.js
@@ -2,7 +2,7 @@ import { fetchWeather } from './fetchWeather';
 import './css/appWeather.css';
 
 
-const weather = document.querySelector('#root'); 
+const weatherEl = document.querySelector('#root'); 
 
 navigator.geolocation.getCurrentPosition(function(position) {
  console.log(position.coords.latitude, position.coords.longitude);
@@ -19,24 +19,7 @@ const App = () => {
 console.log(weather.main.temp, weather.name, Math.round(weather.main.temp), weather.weather[0].icon, weather.weather[0].description, new Date().getDay(), new Date().getDate(), new Date().getFullYear(), new Date().getMonth());
 console.log(`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`);
 
-weather.innerHTML = `
-        <div className="main-container">
-                <div className="city">
-                    <h2 className="city-name">
-                        <span>${weather.name}</span>
-                        <sup>${weather.sys.country}</sup>
-                    </h2>
-                    <div className="city-temp">
-                        ${Math.round(weather.main.temp)}
-                        <sup>&deg;C</sup>
-                    </div>
-                    <div className="info">
-                        <img className="city-icon" src=${`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt=${weather.weather[0].description} />
-                        <p>${weather.weather[0].description}</p>
-                    </div>
-                </div>
-        </div>
-        
+weatherEl.innerHTML = `
         
         <div class="main-container">
 
@@ -64,7 +47,7 @@ weather.innerHTML = `
     </div>
           
               <div class="info-icon">
-                  <img class="city-icon" ${`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt=${weather.weather[0].description}/>
+                  <img class="city-icon" src="${`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}" alt="${weather.weather[0].description}"/>
               </div>
   
               <div>
