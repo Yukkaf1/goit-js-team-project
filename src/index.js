2f656a6ca2aed738f6b922a24cdd58617a3211b9
Trunova Yulia <103053349+Yukkaf1@users.noreply.github.com>
2023-03-02 20:23:36 +0100

    weater 7 day

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 
 import axios from 'axios';
 import weatherApp from './appWeather';
+import fetchWeather7day from './appWeather7day'
+
+fetchWeather7day();
 
 const BASE_URL = 'https://pixabay.com/api/';
 const KEY = '33761438-9314d2b90b41fb92b07a88ae9';
